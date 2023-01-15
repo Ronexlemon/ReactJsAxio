9ee9bcd4e5b82951c58a5e2b684b9f107ee54222
RonexLemon <ronexondimu@gmail.com>
2023-01-15 13:05:09 +0300

    fetch the data

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import axios from "axios";
+import { useState,useEffect } from "react";
 
 const Home=()=>{
-    axios.get("")
+    const [items,setItems] = useState({});
+    const getAllCats = async () => {
+        try {
+            const response = await axios.get("https://cat-fact.herokuapp.com/facts/");
+            setItems(response.data);
+            console.log("the response is", response.data);
+        } catch (error) {
+            console.log("error is",error);
+        }
+    };
+
+
+useEffect(()=>{
+    getAllCats();
+    
+},[])
+
     return(
         <div className="flex flex-row justify-between bg-black  h-screen w-full  text-white ">
-           <div className=" h-1/2 w-1/2 min-h-0  bg-yellow-400 m-10 rounded-2xl ">
-            
-           
+           <div className=" h-1/2 w-1/2 min-h-0   bg-yellow-400 m-10 rounded-2xl ">
+           {items.length > 0 ?
+    items.map((item,index)=>(
+        <div key={item.index} className="grid grid-cols-3   w-full">
+            <h4  >{item.text}</h4><br/>
+        </div>
+        
+    ))
+    : <p>Loading...</p>
+}
 
            </div>
            <div className="flex flex-col justify-between bg-green-400 m-10 h-3/4 w-1/4 rounded-xl">
